0a4bb441b418aeb376e13a265fe13e4dd2aeb436
joseph-tohdjojo <joseph.tohdjojo@gmail.com>
2018-06-11 22:44:10 -0400

    tabs

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,23 +1,23 @@
-( function() {
+(function() {
 
-	var keys = document.querySelectorAll( '.key' );
-	window.addEventListener( 'keydown', playSound );
-	keys.forEach( function( key ) {
-		key.addEventListener( 'transitionend', removeClass );
-	} );
+  var keys = document.querySelectorAll('.key');
+  window.addEventListener('keydown', playSound);
+  keys.forEach(function(key) {
+    key.addEventListener('transitionend', removeClass);
+  });
 
-	function playSound( e ) {
-		var key = document.querySelector( 'div[ data-key="' + e.keyCode + '" ]' );
-		var audio = document.querySelector( 'audio[ data-key="' + e.keyCode + '" ]' );
-		if( !audio ) return;
-		audio.currentTime = 0;
-		audio.play();
-		key.classList.add( 'playing' );
-	}
+  function playSound(e) {
+    var key = document.querySelector('div[data-key="' + e.keyCode + '"]');
+    var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+  }
 
-	function removeClass( e ) {
-		if ( e.propertyName !== 'transform' ) return;
-    e.target.classList.remove( 'playing' );
-	}
+  function removeClass(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
-} )()
+})()
